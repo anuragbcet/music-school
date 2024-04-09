862b523afda63b73f0cd340b47a276f9f10f86f9
Anurag kumar <anuragbcet2023@gmail.com>
2024-04-09 06:44:47 -0700

    project complete

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex
+    flex-col items-center justify-center relative overflow-hidden
+    mx-auto py-10 md:py-0">test page contact
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/courses/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+function page() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex
+    flex-col items-center justify-center relative overflow-hidden
+    mx-auto py-10 md:py-0">test page courses
+    </div>
+  )
+}
+
+export default page
