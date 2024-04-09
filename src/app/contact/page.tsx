f2fcf841cbfd1f4788f35ee8f4758ab970f4bab6
Anurag kumar <anuragbcet2023@gmail.com>
2024-04-09 06:57:47 -0700

    contact page updation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex
     flex-col items-center justify-center relative overflow-hidden
-    mx-auto py-10 md:py-0">test page contact
+    mx-auto py-10 md:py-0">This will get updated soon.
     </div>
   )
 }
