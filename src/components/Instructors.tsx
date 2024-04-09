7ddc2e889de8c1d4ca2a6a135c11b86233199769
Anurag kumar <anuragbcet2023@gmail.com>
2024-04-09 07:20:54 -0700

    text color changed to black

--- src/components/Instructors.tsx
+++ src/components/Instructors.tsx
@@ -37,8 +37,8 @@ function Instructors() {
   return (
     <div className="relative h-[40rem] overflow-hidden flex items-center justify-center ">
         <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
-            <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Meet Our Instructors</h2>
-            <p className="text-base md:text-lg text-white text-center mb-4">Discover the talented professionals who will guide your musical journey</p>
+            <h2 className="text-2xl md:text-4xl lg:text-7xl text-black font-bold text-center mb-8">Meet Our Instructors</h2>
+            <p className="text-base md:text-lg text-black text-center mb-4">Discover the talented professionals who will guide your musical journey</p>
             <div className="flex flex-row items-center justify-center mb-10 w-full">
                 <AnimatedTooltip items={instructors} />
             </div>
