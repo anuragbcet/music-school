0071746cb9a1629e972ac91e67bf9ca0f0365d98
Anurag kumar <anuragbcet2023@gmail.com>
2024-04-12 23:17:18 -0700

    solving errors

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,7 +19,7 @@ function HeroSection() {
             <p className='mt-4 font-normal text-base md:text-lg
             text-neutral-320 max-w-lg mx-auto'
             >Dive into our comprehensive music courses and transform your musical 
-                journey today.Whether you're a begginer or looking to refine your skills ,
+                journey today.Whether you are a begginer or looking to refine your skills ,
                 join us to unlock your true potential.
             </p>
             <div className='mt-4'>
--- src/components/ui/wavy-background.tsx
+++ src/components/ui/wavy-background.tsx
@@ -97,7 +97,7 @@ export const WavyBackground = ({
     return () => {
       cancelAnimationFrame(animationId);
     };
-  }, []);
+  }, [init]);
 
   return (
     <div
