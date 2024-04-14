35b36f91951741b7bd3b5f5da144c2531795c34c
Anurag kumar <anuragbcet2023@gmail.com>
2024-04-13 22:46:11 -0700

    changing the favicon

--- src/components/ui/wavy-background.tsx
+++ src/components/ui/wavy-background.tsx
@@ -85,7 +85,7 @@ export const WavyBackground = ({
 
   let animationId: number;
   const render = () => {
-    ctx.fillStyle = backgroundFill || "black";
+    ctx.fillStyle = backgroundFill || "white";
     ctx.globalAlpha = waveOpacity || 0.5;
     ctx.fillRect(0, 0, w, h);
     drawWave(5);
